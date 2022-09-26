cbbfb575faf36a3abb145f1f252aa5f6f8493e0a
marie <ma.madrigal@estudiantec.cr>
2022-09-26 06:22:41 -0600

    Initial commit

---
+++ src/main/java/mp/mp2509/UiController.java
@@ -0,0 +1,19 @@
+package mp.mp2509;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class UiController implements Initializable {
+
+    @Override
+    public void initialize (URL url, ResourceBundle rb){
+    }
+}
