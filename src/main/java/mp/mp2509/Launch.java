cdfd39af68649e89509b4a36672e7fee2849a968
marie <ma.madrigal@estudiantec.cr>
2022-09-28 04:42:29 -0600

    add applications css

--- src/main/java/mp/mp2509/Launch.java
+++ src/main/java/mp/mp2509/Launch.java
@@ -1,23 +1,53 @@
 package mp.mp2509;
 
+import eu.hansolo.fx.countries.LauncherDemoCountryPane;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import java.util.Objects;
 
-import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Launch extends Application {
+    public static Stage stage=null;
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage)  throws Exception {
+        try{
+            Parent root = FXMLLoader.load (getClass().getResource("Ui.fxml"));
+            Scene scene = new Scene(root);
+            String css= this.getClass().getResource("application.css").toExternalForm();
+            scene.getStylesheets().add(css);
+            //scene.getStylesheets().add(getClass().getResource("stylesheet.css").toExternalForm());
+            scene.setFill(Color.TRANSPARENT);
+            stage.initStyle (StageStyle.TRANSPARENT);
+            stage.setScene(scene);
+            Launch.stage=stage;
+            stage.show();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+    /*
+      @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load (getClass().getResource("resources.Ui.fxml"));
+        Scene scene = new Scene(root);
+        //String css= getClass().getResource("stylesheet.css").toExternalForm();
+        // scene.getStylesheets().add(css);
+        scene.getStylesheets().add(getClass().getResource("stylesheet.css").toExternalForm());
+        scene.setFill(Color.TRANSPARENT);
+        stage.initStyle (StageStyle.TRANSPARENT);
         stage.setScene(scene);
         stage.show();
     }
+     */
 
     public static void main(String[] args) {
-        launch();
-    }
+        launch (args);
+        }
 }
--- src/main/java/mp/mp2509/UiController.java
+++ src/main/java/mp/mp2509/UiController.java
@@ -1,17 +1,18 @@
 package mp.mp2509;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.fxml.Initializable;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.HBox;
+
 
 
 public class UiController implements Initializable {
+
+    @FXML
+    private HBox parent;
 
     @Override
     public void initialize (URL url, ResourceBundle rb){
